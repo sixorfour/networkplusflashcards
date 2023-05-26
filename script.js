7e0162e69071d7265ea5e657d5dec7f7f16a7641
sixorfour <108963250+sixorfour@users.noreply.github.com>
2023-05-25 19:12:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let cardData = [];
 let currentCardIndex = 0;
-let isAnswerDisplayed = false; // Track the answer display state
 
 function parseCSV(csv) {
-  const parsedData = Papa.parse(csv, { header: true });
-  cardData = parsedData.data;
+  const lines = csv.split('\n');
+  const headers = lines[0].split(',');
+
+  const cardData = [];
+  for (let i = 1; i < lines.length; i++) {
+    const values = lines[i].split(',');
+    if (values.length !== headers.length) {
+      continue; // Skip invalid rows with mismatched values
+    }
+    const card = {};
+
+    for (let j = 0; j < headers.length; j++) {
+      const value = values[j].trim();
+      card[headers[j]] = value;
+    }
+
+    cardData.push(card);
+  }
+
   return cardData;
 }
 
@@ -16,41 +32,29 @@ function showCard(index) {
   const answerContent = document.getElementById('answer-content');
 
   questionContent.textContent = card.question;
-  answerContent.textContent = card.answer;
+  answerContent.textContent = card.answer; // Display the answer content
 
-  if (isAnswerDisplayed) {
-    questionHeader.textContent = ''; // Hide the question header
-    answerHeader.textContent = 'Answer';
-  } else {
-    questionHeader.textContent = 'Question';
-    answerHeader.textContent = '';
-  }
+  questionHeader.textContent = 'Question';
+  answerHeader.textContent = 'Answer';
 }
 
 function flipCard() {
   const cardContainer = document.querySelector('.card-container');
   cardContainer.classList.toggle('flip');
-  isAnswerDisplayed = !isAnswerDisplayed; // Toggle the answer display state
 }
 
 function nextCard() {
   currentCardIndex = (currentCardIndex + 1) % cardData.length;
-  isAnswerDisplayed = false; // Reset the answer display state
-  const cardContainer = document.querySelector('.card-container');
-  cardContainer.classList.remove('flip'); // Ensure the question side is shown
   showCard(currentCardIndex);
 }
 
-fetch('flashcards.csv')
-  .then(response => response.text())
-  .then(data => {
-    cardData = parseCSV(data).then(parsedData => {
-      cardData = parsedData;
-      showCard(currentCardIndex);
-    }).catch(error => console.log('Error parsing CSV:', error));
+function extCSV(csvData) {
+  console.log(csvData);
+  (data => {
+    cardData = parseCSV(data);
+    showCard(currentCardIndex);
   })
-  .catch(error => console.log('Error fetching CSV:', error));
+}
 
-
-document.getElementById('flip-button').addEventListener('click', flipCard);
+document.getElementById('flip-button').addEventListener('click', () => flipCard());
 document.getElementById('next-button').addEventListener('click', nextCard);
